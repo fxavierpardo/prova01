f9f17f2d1dee70eefcf99be27e803fe5349365b6
Xavier Pardo <xavier@macbook-pro-de-pilar.local>
2016-01-09 22:03:19 +0100

    Initial Commit

---
+++ prova01a/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  prova01a
+//
+//  Created by Xavier Pardo on 9/1/16.
+//  Copyright © 2016 Xavier Pardo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ prova01a/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  prova01a
+//
+//  Created by Xavier Pardo on 9/1/16.
+//  Copyright © 2016 Xavier Pardo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
